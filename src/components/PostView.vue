9660e7b9c040391f796351ccc94c42683b3c2b0f
kingonly <31890660+kingonly@users.noreply.github.com>
2017-09-14 00:14:11 +0300

    improved label

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -8,8 +8,8 @@
       </div>
       <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
-          <label for="username" class="mdl-textfield__label">Describe me</label>
+          <input id="title" v-model="title" type="text" class="mdl-textfield__input"/>
+          <label for="title" class="mdl-textfield__label">Describe me</label>
         </div>
         <div class="actions">
           <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
